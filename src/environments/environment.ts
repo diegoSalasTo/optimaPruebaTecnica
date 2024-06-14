73c85c451536d2a126cfa0e1214516bafdaa0287
diegoSalasTo <113932427+diegoSalasTo@users.noreply.github.com>
2024-06-14 15:03:29 -0500

    Update api_key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     //Url Api
     urlApi: "https://api.giphy.com/v1/",
     //Login
-    api_key:"KQOJVC3ooeUhiSAUQwWWRdYRLMjBqYE8",
+    api_key:"A3wKON4vNI0fbonNRhQaW95AmW99aoTI",
   };
